0cde3317f74093450194627472f18fec70c6dbce
Samba Diallo <samba.diallo@id-mac007.idtgv.cnit>
2021-05-22 17:13:29 +0200

    MacBook-Pro:react-testing-library

--- src/__tests__/asynchronous.test.js
+++ src/__tests__/asynchronous.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
-import { render, screen } from '@testing-library/react';
+//  import { render, screen } from '@testing-library/react';
+import { render, screen, act } from '@testing-library/react'; // to use act
 import userEvent from '@testing-library/user-event';
  
 import Asynchronous from '../asynchronous';
@@ -14,16 +15,41 @@ describe('App', () => {
       { objectID: '2', title: 'React' },
     ];
 
-    axios.get.mockImplementationOnce(() =>
+   axios.get.mockImplementationOnce(() =>
       Promise.resolve({ data: { hits: stories } })
     );
     render(<Asynchronous />);
 
     await userEvent.click(screen.getByRole('button'));
 
+
+  //   await act(() =>  axios.get.mockImplementationOnce(() =>
+  //   Promise.resolve({ data: { hits: stories } })
+  // ));
+
     const items = await screen.findAllByRole('listitem');
     expect(items).toHaveLength(2);
   });
+
+  test('fetches stories from an API and displays them with act', async () => {
+    const stories = [
+      { objectID: '1', title: 'Hello' },
+      { objectID: '2', title: 'React' },
+    ];
+
+    const promise = Promise.resolve({ data: { hits: stories } });
+
+    axios.get.mockImplementationOnce(() => promise);
+
+    render(<Asynchronous />);
+
+    await userEvent.click(screen.getByRole('button'));
+
+    await act(() => promise);
+
+    expect(screen.getAllByRole('listitem')).toHaveLength(2);
+  });
+
 
   test('fetches stories from an API and fails', async () => {
     axios.get.mockImplementationOnce(() =>
@@ -38,4 +64,6 @@ describe('App', () => {
 
     expect(message).toBeInTheDocument();
   });
+
+
 });
